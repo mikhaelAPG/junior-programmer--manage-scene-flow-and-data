ae2defc4c3b90db5f242d53b57f0790071d1441b
Mikhael Adriel <53480201+mikhaelAPG@users.noreply.github.com>
2021-11-08 23:21:42 +0700

    initial commit
    Write a method to load the Main scene

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
+
 public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
@@ -18,7 +20,12 @@ public class MenuUIHandler : MonoBehaviour
     private void Start()
     {
         ColorPicker.Init();
-        //this will call the NewColorSelected function when the color picker have a color button clicked.
+        // this will call the NewColorSelected function when the color picker has a color button clicked
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+    
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
     }
 }
