74afd5ba03af1bda40d7012ac899c4f09dd37df3
Mikhael Adriel <53480201+mikhaelAPG@users.noreply.github.com>
2021-11-09 11:28:35 +0700

    Implement data persistence between sessions
    Add SaveColor and LoadColor Method

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -18,6 +18,7 @@ public class MenuUIHandler : MonoBehaviour
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
+        MainManager.Instance.TeamColor = color;
     }
     
     private void Start()
@@ -25,6 +26,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         // this will call the NewColorSelected function when the color picker has a color button clicked
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
     
     public void StartNew()
@@ -39,5 +41,17 @@ public class MenuUIHandler : MonoBehaviour
         #else
             Application.Quit(); // original code to quit Unity player
         #endif
+        MainManager.Instance.SaveColor(); 
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
